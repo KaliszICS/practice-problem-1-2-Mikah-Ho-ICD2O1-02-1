4c88ae48dfdee6f1cf3c22a34b3853179a0f9345
Mikah-Ho-ICD2O1-02 <mikah.ho@student.tdsb.on.ca>
2026-02-11 16:24:19 +0000

    wwwww

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,22 +10,77 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+
+		int two;
+		int five;
+		int three;
+
+		two = 2;
+		five = 5;
+		three = 3;
+
+		int sum;
+		int product;
+
+		sum = two + five + three;
+		product = two * five * three;
+
+		System.out.println(sum);
+		System.out.println(product);
+
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+
+		int ten;
+		int three;
+
+		ten = 10;
+		three = 3;
+		
+		System.out.println(ten / three);
+		System.out.println(ten % three);
+
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+
+		double num1;
+		double num2;
+
+		num1 = 5.3;
+		num2 = 5.2;
+
+		System.out.println(num1 / num2);
+		
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+
+		int two;
+		int three; 
+		int four; 
+
+		two = 2;
+		three = 3;
+		four = 4;
+
+		System.out.println((two + three) * four);
+
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+
+		int five;
+
+		five = 5;
+
+		System.out.println(five + 3.5);
+
 	}
 
 }
